c5f2416ad843a7186536cc4f813b564e85060bf5
davekcd <github@david-bush.co.uk>
2015-08-12 19:30:11 +1000

    Initial commit.

---
+++ src/slacko/core.clj
@@ -0,0 +1,7 @@
+(ns slacko.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/slacko/core_test.clj
@@ -0,0 +1,7 @@
+(ns slacko.core-test
+  (:require [clojure.test :refer :all]
+            [slacko.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
