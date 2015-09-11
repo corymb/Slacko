c609e1f7707295fd8712bc10e93cc78dc45d9c87
Dave Bush <github@david-bush.co.uk>
2015-09-12 08:27:09 +1000

    Rebased from master to integrate API communication.

--- src/slacko/core.clj
+++ src/slacko/core.clj
@@ -2,7 +2,7 @@
   (:gen-class))
 
 (use 'slack-rtm.core)
-(def rtm-conn (connect ""))
+(def rtm-conn (connect "xoxb-10539963701-hosor4JglBTaEn1ZPoQvtWuJ"))
 
 (def events-publication (:events-publication rtm-conn))
 
@@ -11,8 +11,44 @@
 
 (def message-receiver message-handler)
 
+(use '[clojure.java.shell :only [sh]])
+(require '[clojure.string :as str])
 
+(defn parse-deploy-string [deploy-string]
+  (zipmap [:deploy :target :branch] (str/split deploy-string #" "))
+)
+
+(defn deploy [command]
+  (println (parse-deploy-string command))
+  (println "Deploying...")
+  ;; (get :errors (sh command) "Deploy successful.")
+  (get :errors (sh "ls") "Deploy successful.")
+)
+
+(defn dispatcher [func args]
+  (let [args (subs args 1) commands
+      { :deploy (deploy args), }] 
+  (func commands))
+)
+
+(defn message-handler [message]
+  "Parses mesages for keywords."
+  (case (re-find #"^!([\w]*)" message)
+    "!deploy" (dispatcher :deploy message)
+    nil
+  )
+)
+
+;; (defn send-message [channel]
+;;   "Sends a message to the channel."
+;;   (println (conj "Hi" message))
+;; )
 (defn -main
   ;; (println (System/getenv "SLACK_TOKEN")))
   [& args]
-  (sub-to-event events-publication :message message-receiver))
+  (sub-to-event events-publication :message message-receiver)
+  ;; (-> (deploy "ls") println )
+
+  ;; (println (:deploy dispatcher))
+  (println (message-handler "!deploy staging master"))
+)
--- test/slacko/core_test.clj
+++ test/slacko/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [slacko.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest successful-deploy
+  (testing "Successful deployment."
+    (is (= (deploy "deploy string") "Deploy successful."))))
+
+(deftest parse-deploy-string-without-branch
+  (testing "Deploy string parsing without branch."
+    (is (= (count (parse-deploy-string "deploy staging")) 2))
+    (is (= (:target (parse-deploy-string "deploy staging")) "staging"))
+    (is (= (:branch (parse-deploy-string "deploy staging") nil) nil))))
+
+(deftest parse-deploy-string-with-branch
+  (testing "Deploy string parsing with branch."
+    (is (= (count (parse-deploy-string "deploy staging test-branch")) 3))
+    (is (= (:target (parse-deploy-string "deploy staging")) "staging"))
+    (is (= (:branch (parse-deploy-string "deploy staging test-branch")) "test-branch"))))
