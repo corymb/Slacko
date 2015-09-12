92529f7b1366c64a5fbbd46fefbab2dc932f8c78
Dave Bush <github@david-bush.co.uk>
2015-09-12 12:04:45 +1000

    Added bot reply functionality.

--- src/slacko/core.clj
+++ src/slacko/core.clj
@@ -8,16 +8,24 @@
 (if-let
   [token (System/getenv "SLACK_TOKEN")]
   (def rtm-conn (connect token))
-  (throw (Exception. "Set API key as $SLACK_TOKEN environment variable.")))
+  (throw (Exception. "Set API key as $SLACK_TOKEN environment variable")))
 
 
 (defn parse-deploy-string [deploy-string]
   (zipmap [:deploy :target :branch] (str/split deploy-string #" "))
 )
 
+(defn send-message [message]
+  "Sends a message to the channel."
+  (send-event (:dispatcher rtm-conn)
+              {:type "message"
+               :text message
+               :channel "C0AFV93U6"
+               :user "U0AFVUBLM"}))
+
 (defn deploy [message]
   (let [vars (parse-deploy-string (:text message))]
-  (get :errors (sh "/bin/sh" "deploy.sh" (:target vars) (:branch vars) "Deploy successful.")))
+  (get :errors (sh "/bin/sh" "deploy.sh" (:target vars) (:branch vars) (send-message "Deploy successful.")))))
 
 (defn message-handler [message]
   ;; Routes commands to functions
@@ -26,14 +34,8 @@
       "!deploy" (deploy message)
       nil)))
 
-
 (def events-publication (:events-publication rtm-conn))
 (def message-receiver message-handler)
-
-;; (defn send-message [channel]
-;;   "Sends a message to the channel."
-;;   (println (conj "Hi" message))
-;; )
 
 (defn -main
   [& args]
