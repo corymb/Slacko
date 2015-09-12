edbb88248f6d2a6d2b95bea41f8bedf82e14de4c
Dave Bush <github@david-bush.co.uk>
2015-09-12 11:23:34 +1000

    Added deployment handler.

--- src/slacko/core.clj
+++ src/slacko/core.clj
@@ -10,34 +10,31 @@
   (def rtm-conn (connect token))
   (throw (Exception. "Set API key as $SLACK_TOKEN environment variable.")))
 
-(def events-publication (:events-publication rtm-conn))
 
 (defn parse-deploy-string [deploy-string]
   (zipmap [:deploy :target :branch] (str/split deploy-string #" "))
 )
 
-(defn deploy [command]
-  (println (parse-deploy-string command))
-  (println "Deploying...")
-  ;; (get :errors (sh command) "Deploy successful.")
-  (get :errors (sh "ls") "Deploy successful.")
-)
+;; (defn dispatcher [func args]
+  ;; (let [args (subs args 1) commands
+  ;;     { :deploy (deploy args), }]
+  ;; (func commands))
+;; )
 
-(defn dispatcher [func args]
-  (let [args (subs args 1) commands
-      { :deploy (deploy args), }]
-  (func commands))
-)
+(defn deploy [message]
+  (let [vars (parse-deploy-string (:text message))]
+  (get :errors (sh "/bin/sh" "deploy.sh" (:target vars) (:branch vars) "Deploy successful.")))
 
 (defn message-handler [message]
-  (case (re-find #"^!([\w]*)" message)
-    "!deploy" (dispatcher :deploy message)
-    nil
-  )
-  (println [123 (:text message)]))
+  ;; Routes commands to functions
+  (let [message-text (:text message)]
+    (case (first (re-find #"^!([\w]*)" message-text))
+      "!deploy" (deploy message)
+      nil)))
 
+
+(def events-publication (:events-publication rtm-conn))
 (def message-receiver message-handler)
-
 
 ;; (defn send-message [channel]
 ;;   "Sends a message to the channel."
@@ -46,10 +43,6 @@
 
 (defn -main
   [& args]
-  (sub-to-event events-publication :message message-receiver)
-  ;; (-> (deploy "ls") println )
-
-  ;; (println (:deploy dispatcher))
-  ;; (println (message-handler "!deploy staging master"))
-  ;; (println rtm-conn)
+  ;; (sub-to-event events-publication :message message-receiver)
+  (println (deploy {:text "!deploy staging" :dave "PEW"}))
 )
